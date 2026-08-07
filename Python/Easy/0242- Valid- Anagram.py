e87bdeb046ecaed13d150e5b0e6f28e095fcc8aa
Pratyush kumar <pratyushdubey006@gmail.com>
2026-08-07 18:52:34 +0530

    Add isAnagram method to check for anagrams

---
+++ Python/Easy/0242- Valid- Anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+       if len(s) != len(t):
+        return False
+       d1 = {}
+       d2 = {}
+       for i in s:
+           d1[i] = d1.get(i,0)+1
+       for i in t:
+           d2[i] = d2.get(i,0)+1
+       if d1 == d2:
+           return True
+       else:
+           return False
